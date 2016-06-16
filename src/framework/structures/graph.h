eaa8d9c32c5630f8b6e91b89ee9612ab4f866503
Iván Pondal <ivan.pondal@gmail.com>
2016-06-16 00:39:59 -0300

    Refactor graph printing to be independent of implementation

--- src/framework/structures/graph.h
+++ src/framework/structures/graph.h
@@ -17,9 +17,30 @@ class graph{
 		virtual void add_edge(const T &v1, const T &v2) = 0;
 		virtual bool contains(const T &v) const = 0;
 		virtual graph<T> *clone() const = 0;
-		virtual void join(const graph<T>& g) = 0;
-		virtual void unite(const graph<T>& g) = 0;
+		virtual void join(const graph<T> &g) = 0;
+		virtual void unite(const graph<T> &g) = 0;
 		virtual const std::vector<T> &get_vertices() const = 0;
 };
+
+template <typename T>
+std::istream &operator>>(std::istream &input, graph<T> &g){
+	int m = 0;
+	input >> m >> m;
+
+	T tmp_a, tmp_b;
+
+	for(int i = 0; i < m; i++){
+		input >> tmp_a >> tmp_b;
+		if(!g.contains(tmp_a)){
+			g.add_node(tmp_a);
+		}
+		if(!g.contains(tmp_b)){
+			g.add_node(tmp_b);
+		}
+		g.add_edge(tmp_a, tmp_b);
+	}
+
+	return input;
+}
 
 #endif // GRAPH_H_INCLUDED
