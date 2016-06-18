69a9f608249b965cddce3764d90b1d961f48a7f7
Franco Frizzo <francofrizzo@gmail.com>
2016-06-18 00:36:28 -0300

    Add dummy 'test.cpp' to Ex 3

---
+++ src/ej3/test.cpp
@@ -0,0 +1,17 @@
+#include "../main.h"
+#include "./ej3.h"
+#include "../mini_test.h"
+#include "../framework/structures/adj_list_graph.h"
+
+#include <algorithm>
+
+/*
+**  Exercise 3
+**  Unit tests
+*/
+
+using namespace std;
+
+void run_unit_tests() {
+   std::cout << "Nothing here yet :/" << std::endl;
+}
