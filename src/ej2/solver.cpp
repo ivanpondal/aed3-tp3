a4442052ea28c3573f541d5c02617055aabb8adb
manumena <manuelmena1993@gmail.com>
2016-06-19 17:24:35 -0300

    Exercise 2 with some prunes

---
+++ src/ej2/solver.cpp
@@ -0,0 +1,141 @@
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include <unistd.h>
+#include "../framework/structures/graph.h"
+#include "../framework/structures/adj_list_graph.h"
+#include "../main.h"
+#include "ej2.h"
+
+using namespace std;
+
+int podas = 0;
+int super_podas = 0;
+
+int degree_sum(graph<int> *g, vector<int> remaining_nodes) {
+    int sum = 0;
+    for (vector<int>::iterator it = remaining_nodes.begin(); it != remaining_nodes.end(); it++) {
+        sum += g->degree(*it);
+    }
+    return sum;
+}
+
+bool mcs_backtracking(
+        graph<int> *g1,
+        graph<int> *g2,
+        graph<int> *subgraph,
+        unordered_map<int, int> node_map,
+        vector<int> remaining_g1_nodes,
+        vector<int> remaining_g2_nodes,
+        int &maxEdges,
+        unordered_set<tuple<int, int, int, int>, hash_tuple_int> &permutations
+) {
+
+    if (subgraph->m() + degree_sum(g1, remaining_g1_nodes) <= maxEdges) {
+        podas++;
+        return false;
+    }
+
+    if (!remaining_g1_nodes.empty() && !remaining_g2_nodes.empty()) {
+
+        graph<int> *max_subgraph = new adj_list_graph<int>();
+
+        for (unsigned int remaining_g1_index = 0; remaining_g1_index < remaining_g1_nodes.size(); remaining_g1_index++) {
+            for (unsigned int remaining_g2_index = 0; remaining_g2_index < remaining_g2_nodes.size(); remaining_g2_index++) {
+
+                int g1_node = remaining_g1_nodes[remaining_g1_index];
+                int g2_node = remaining_g2_nodes[remaining_g2_index];
+                // cout << "(" << setfill(' ') << setw(2) <<  g1_node << "," << setfill(' ') << setw(2) << g2_node << ") " << flush;
+
+                // Verify if actual mapping can be permuted with a previous branch (only if it is the second mapping)
+                if (subgraph->n() == 1) {
+                    unordered_map<int, int>::iterator it = node_map.begin();
+                    tuple<int, int, int, int> permutation(it->first, it->second, g1_node, g2_node);
+                    
+                    if (permutations.find(permutation) != permutations.end()) {
+                        super_podas++;
+                        // cout << "\b\b\b\b\b\b\b\b" << flush;
+                        continue;
+                    }
+                    else {
+                        tuple<int, int, int, int> reverse_permutation(g1_node, g2_node, it->first, it->second);
+                        permutations.insert(reverse_permutation);
+                    }
+                }
+
+                // Copy remaining nodes and erase actual nodes from the copies
+                vector<int> remaining_g1_nodes_copy = remaining_g1_nodes;
+                vector<int> remaining_g2_nodes_copy = remaining_g2_nodes;
+                remaining_g1_nodes_copy.erase(remaining_g1_nodes_copy.begin() + remaining_g1_index);
+                remaining_g2_nodes_copy.erase(remaining_g2_nodes_copy.begin() + remaining_g2_index);
+
+                // Map nodes
+                unordered_map<int, int> node_map_copy = node_map;
+                pair<int, int> par(g1_node,g2_node);
+                node_map_copy.insert(par);
+
+                // Add new node to subgraph
+                graph<int> *subgraph_copy = subgraph->clone();
+                subgraph_copy->add_node(g1_node);
+
+                // Add edges to the existing neighbours
+                vector<int> g1_neighbours = g1->neighbours(g1_node);
+                for (unsigned int neighbour_index = 0; neighbour_index < g1_neighbours.size(); neighbour_index++) {
+
+                    int g1_neighbour = g1_neighbours[neighbour_index];
+
+                    // Check if the neighbour is in the subgraph
+                    if (subgraph->contains(g1_neighbour)) {
+                        // Check if the neighbour is mapped to a neighbour of the remaining_g2 node-to-map
+                        int g2_mapped_node = node_map.at(g1_neighbour);
+                        if (g2->adjacent(g2_mapped_node, g2_node)) {
+                            // Add edge
+                            subgraph_copy->add_edge(g1_node, g1_neighbour);
+                        }
+                    }
+                }
+
+                bool valid_subgraph = mcs_backtracking(g1, g2, subgraph_copy, node_map_copy, remaining_g1_nodes_copy, remaining_g2_nodes_copy, maxEdges, permutations);
+
+                if (valid_subgraph && subgraph_copy->m() >= max_subgraph->m()) {
+                    *max_subgraph = *subgraph_copy;
+                    if (max_subgraph->m() > maxEdges)
+                        maxEdges = max_subgraph->m();
+                }
+
+                // cout << "\b\b\b\b\b\b\b\b" << flush;
+            }
+        }
+
+        *subgraph = *max_subgraph;
+    }
+
+    return true;
+}
+
+graph<int>* mcs(graph<int> *g1, graph<int> *g2) {
+    adj_list_graph<int> *empty_graph = new adj_list_graph<int>();
+    unordered_map<int, int> empty_map;
+    unordered_set<tuple<int, int, int, int>, hash_tuple_int> permutations;
+    int edges = 0;
+
+    mcs_backtracking(g1, g2, empty_graph, empty_map, g1->get_vertices(), g2->get_vertices(), edges, permutations);
+
+    cout << endl << "podas: " << podas << endl;
+    cout << "super_podas: " << super_podas << endl;
+    
+    // tuple<int, int, int, int> t(1, 2, 3, 4);
+    // permutations.insert(t);
+    // tuple<int, int, int, int> a(1, 2, 3, 5);
+    // permutations.insert(a);
+    // for (unordered_set<tuple<int, int, int, int>, hash_tuple_int>::iterator it = permutations.begin(); it != permutations.end(); it++){
+    //     cout << get<0>(*it) << get<1>(*it) << get<2>(*it) << get<3>(*it) << endl;
+    // }
+    
+    return empty_graph;
+}
+
+solution run_solver(graph<int> &g1, graph<int> &g2) {
+
+
+}
---
+++ src/ej2/test.cpp
@@ -0,0 +1,350 @@
+#include "../main.h"
+#include "ej2.h"
+#include "../mini_test.h"
+#include "../framework/structures/adj_list_graph.h"
+#include "../framework/utils/element_generator_int.h"
+#include "../framework/utils/graph_factory.h"
+#include <sstream>
+
+using namespace std;
+
+void test_isolated_nodes() {
+    adj_list_graph<int> g1;
+    adj_list_graph<int> g2;
+
+    g1.add_node(1);
+    g1.add_node(2);
+    g2.add_node(2);
+    g2.add_node(3);
+
+    graph<int> *maxComSub = mcs(&g1, &g2);
+
+    ASSERT_EQ(maxComSub->m(), 0);
+}
+
+void test_k1_and_other() {
+    adj_list_graph<int> g1;
+    adj_list_graph<int> g2;
+
+    g1.add_node(1);
+    g2.add_node(2);
+    g2.add_node(1);
+
+    graph<int> *maxComSub = mcs(&g1, &g2);
+
+    ASSERT_EQ(maxComSub->m(), 0);
+}
+
+void test_k2s() {
+    adj_list_graph<int> g1;
+    adj_list_graph<int> g2;
+
+    g1.add_node(1);
+    g1.add_node(2);
+    g1.add_edge(1, 2);
+
+    g2.add_node(3);
+    g2.add_node(4);
+    g2.add_edge(3, 4);
+
+
+    graph<int> *maxComSub = mcs(&g1, &g2);
+
+    ASSERT_EQ(maxComSub->m(), 1);
+    ASSERT_EQ(maxComSub->n(), 2);
+}
+
+void test_k2_and_k3() {
+    element_generator_int e_gen;
+    adj_list_graph<int> k2, k3;
+
+    graph_factory<int>::add_n_random_vertices(k2, e_gen, 2, 1.0f);
+    graph_factory<int>::add_n_random_vertices(k3, e_gen, 3, 1.0f);
+
+    graph<int> *maxComSub = mcs(&k2, &k3);
+
+    ASSERT_EQ(maxComSub->m(), 1);
+    ASSERT_EQ(maxComSub->n(), 2);
+}
+
+void test_k4s() {
+    element_generator_int e_gen;
+    adj_list_graph<int> k4, k4bis;
+
+    graph_factory<int>::add_n_random_vertices(k4, e_gen, 4, 1.0f);
+    graph_factory<int>::add_n_random_vertices(k4bis, e_gen, 4, 1.0f);
+
+    graph<int> *maxComSub = mcs(&k4, &k4bis);
+
+    ASSERT_EQ(maxComSub->m(), 6);
+    ASSERT_EQ(maxComSub->n(), 4);
+}
+
+void test_p4_and_2k2() {
+    adj_list_graph<int> p4;
+    adj_list_graph<int> k2s;
+
+    p4.add_node(1);
+    p4.add_node(2);
+    p4.add_node(3);
+    p4.add_node(4);
+    p4.add_edge(1, 2);
+    p4.add_edge(2, 3);
+    p4.add_edge(3, 4);
+
+    k2s.add_node(1);
+    k2s.add_node(2);
+    k2s.add_node(3);
+    k2s.add_node(4);
+    k2s.add_edge(1, 2);
+    k2s.add_edge(3, 4);
+
+    graph<int> *maxComSub = mcs(&p4, &k2s);
+
+    ASSERT_EQ(maxComSub->m(), 2);
+}
+
+void test_p4_and_3k2() {
+    adj_list_graph<int> p4;
+    adj_list_graph<int> k2s;
+
+    p4.add_node(1);
+    p4.add_node(2);
+    p4.add_node(3);
+    p4.add_node(4);
+    p4.add_edge(1, 2);
+    p4.add_edge(2, 3);
+    p4.add_edge(3, 4);
+
+    k2s.add_node(1);
+    k2s.add_node(2);
+    k2s.add_node(3);
+    k2s.add_node(4);
+    k2s.add_node(5);
+    k2s.add_node(6);
+    k2s.add_edge(1, 2);
+    k2s.add_edge(3, 4);
+    k2s.add_edge(5, 6);
+
+    graph<int> *maxComSub = mcs(&p4, &k2s);
+
+    ASSERT_EQ(maxComSub->m(), 2);
+}
+
+void test_handcrafted_and_p5() {
+    adj_list_graph<int> handcrafted;
+    adj_list_graph<int> p5;
+
+    handcrafted.add_node(1);
+    handcrafted.add_node(2);
+    handcrafted.add_node(3);
+    handcrafted.add_node(4);
+    handcrafted.add_edge(1, 2);
+    handcrafted.add_edge(2, 3);
+    handcrafted.add_edge(2, 4);
+    handcrafted.add_edge(3, 4);
+
+    p5.add_node(1);
+    p5.add_node(2);
+    p5.add_node(3);
+    p5.add_node(4);
+    p5.add_node(5);
+    p5.add_edge(1, 2);
+    p5.add_edge(2, 3);
+    p5.add_edge(3, 4);
+    p5.add_edge(4, 5);
+
+    graph<int> *maxComSub = mcs(&handcrafted, &p5);
+
+    ASSERT_EQ(maxComSub->m(), 3);
+}
+
+void test_handcrafteds() {
+    adj_list_graph<int> graph1, graph2;
+
+    graph1.add_node(1);
+    graph1.add_node(2);
+    graph1.add_node(3);
+    graph1.add_node(4);
+    graph1.add_node(5);
+    graph1.add_edge(1, 2);
+    graph1.add_edge(1, 4);
+    graph1.add_edge(1, 5);
+    graph1.add_edge(2, 3);
+    graph1.add_edge(2, 4);
+    graph1.add_edge(3, 4);
+    graph1.add_edge(4, 5);
+
+    graph2.add_node(1);
+    graph2.add_node(2);
+    graph2.add_node(3);
+    graph2.add_node(4);
+    graph2.add_node(5);
+    graph2.add_node(6);
+    graph2.add_edge(1, 2);
+    graph2.add_edge(1, 4);
+    graph2.add_edge(1, 6);
+    graph2.add_edge(2, 3);
+    graph2.add_edge(2, 4);
+    graph2.add_edge(3, 4);
+    graph2.add_edge(4, 5);
+
+    graph<int> *maxComSub = mcs(&graph1, &graph2);
+
+    ASSERT_EQ(maxComSub->m(), 6);
+}
+
+void test_pseudofranco_example() {
+    adj_list_graph<int> graph1, graph2;
+
+    graph1.add_node(1);
+    graph1.add_node(2);
+    graph1.add_node(3);
+    graph1.add_node(4);
+    graph1.add_node(5);
+    graph1.add_node(6);
+    graph1.add_edge(1, 2);
+    graph1.add_edge(1, 3);
+    graph1.add_edge(2, 3);
+    graph1.add_edge(3, 4);
+    graph1.add_edge(4, 5);
+    graph1.add_edge(4, 6);
+    graph1.add_edge(5, 6);
+
+    graph2.add_node(1);
+    graph2.add_node(2);
+    graph2.add_node(3);
+    graph2.add_node(4);
+    graph2.add_node(5);
+    graph2.add_node(6);
+    graph2.add_node(7);
+    graph2.add_edge(1, 2);
+    graph2.add_edge(1, 3);
+    graph2.add_edge(2, 3);
+    graph2.add_edge(3, 4);
+    graph2.add_edge(4, 5);
+    graph2.add_edge(5, 6);
+    graph2.add_edge(5, 7);
+    graph2.add_edge(6, 7);
+
+    graph<int> *maxComSub = mcs(&graph1, &graph2);
+
+    ASSERT_EQ(maxComSub->m(), 6);
+}
+
+void test_pseudofranco_example2() {
+    adj_list_graph<int> franco1, franco2;
+
+    franco1.add_node(1);
+    franco1.add_node(2);
+    franco1.add_node(3);
+    franco1.add_node(4);
+    franco1.add_node(5);
+    franco1.add_node(6);
+    franco1.add_node(7);
+    franco1.add_edge(1, 2);
+    franco1.add_edge(1, 3);
+    franco1.add_edge(1, 4);
+    franco1.add_edge(2, 3);
+    franco1.add_edge(2, 4);
+    franco1.add_edge(3, 4);
+    franco1.add_edge(2, 5);
+    franco1.add_edge(5, 6);
+    franco1.add_edge(5, 7);
+    franco1.add_edge(6, 7);
+
+    franco2.add_node(1);
+    franco2.add_node(2);
+    franco2.add_node(3);
+    franco2.add_node(4);
+    franco2.add_node(5);
+    franco2.add_node(6);
+    franco2.add_node(7);
+    franco2.add_node(8);
+    franco2.add_edge(1, 2);
+    franco2.add_edge(1, 3);
+    franco2.add_edge(1, 4);
+    franco2.add_edge(2, 3);
+    franco2.add_edge(2, 4);
+    franco2.add_edge(3, 4);
+    franco2.add_edge(2, 8);
+    franco2.add_edge(8, 5);
+    franco2.add_edge(5, 6);
+    franco2.add_edge(5, 7);
+    franco2.add_edge(6, 7);
+
+    graph<int> *maxComSub = mcs(&franco1, &franco2);
+
+    ASSERT_EQ(maxComSub->m(), 9);
+}
+
+void test_franco_example() {
+    adj_list_graph<int> franco1, franco2;
+
+    franco1.add_node(1);
+    franco1.add_node(2);
+    franco1.add_node(3);
+    franco1.add_node(4);
+    franco1.add_node(5);
+    franco1.add_node(6);
+    franco1.add_node(7);
+    franco1.add_node(8);
+    franco1.add_edge(1, 2);
+    franco1.add_edge(1, 3);
+    franco1.add_edge(1, 4);
+    franco1.add_edge(2, 3);
+    franco1.add_edge(2, 4);
+    franco1.add_edge(3, 4);
+    franco1.add_edge(2, 5);
+    franco1.add_edge(5, 6);
+    franco1.add_edge(5, 7);
+    franco1.add_edge(5, 8);
+    franco1.add_edge(6, 7);
+    franco1.add_edge(6, 8);
+    franco1.add_edge(7, 8);
+
+    franco2.add_node(1);
+    franco2.add_node(2);
+    franco2.add_node(3);
+    franco2.add_node(4);
+    franco2.add_node(5);
+    franco2.add_node(6);
+    franco2.add_node(7);
+    franco2.add_node(8);
+    franco2.add_node(9);
+    franco2.add_edge(1, 2);
+    franco2.add_edge(1, 3);
+    franco2.add_edge(1, 4);
+    franco2.add_edge(2, 3);
+    franco2.add_edge(2, 4);
+    franco2.add_edge(3, 4);
+    franco2.add_edge(2, 9);
+    franco2.add_edge(9, 5);
+    franco2.add_edge(5, 6);
+    franco2.add_edge(5, 7);
+    franco2.add_edge(5, 8);
+    franco2.add_edge(6, 7);
+    franco2.add_edge(6, 8);
+    franco2.add_edge(7, 8);
+
+    graph<int> *maxComSub = mcs(&franco1, &franco2);
+
+    cout << endl << *maxComSub << endl;
+
+    ASSERT_EQ(maxComSub->m(), 12);
+}
+
+void run_unit_tests() {
+    // RUN_TEST(test_isolated_nodes);
+    // RUN_TEST(test_k1_and_other);
+    RUN_TEST(test_k2s);
+    RUN_TEST(test_k2_and_k3);
+    RUN_TEST(test_k4s);
+    RUN_TEST(test_p4_and_2k2);
+    RUN_TEST(test_p4_and_3k2);
+    RUN_TEST(test_handcrafted_and_p5);
+    RUN_TEST(test_handcrafteds);
+    RUN_TEST(test_pseudofranco_example);
+    RUN_TEST(test_pseudofranco_example2);
+    RUN_TEST(test_franco_example);
+}
