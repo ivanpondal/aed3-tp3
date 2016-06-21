feaf27284e8fc9c070dde4e85192a7d7c67a9209
manumena <manuelmena1993@gmail.com>
2016-06-21 08:27:38 -0300

    Exercise 2 exp in progress

---
+++ src/ej2/experimentation.cpp
@@ -0,0 +1,25 @@
+// #include "../main.h"
+// #include "./ej2.h"
+
+// void adj_list_n_incremental_experiment::load_instance(incremental_experiment_input<int, adj_list_graph<int>> *input){
+// 	element_generator_int e_gen1, e_gen2;
+// 	graph_factory<int>::add_n_random_vertices(g1, e_gen1, delta_val, 0.5f);
+// 	graph_factory<int>::add_n_random_vertices(g2, e_gen2, delta_val, 0.5f);
+// }
+
+// void adj_list_n_incremental_experiment::solve_instance(incremental_experiment_input<int, adj_list_graph<int>> *input){
+// 	mcs(&g1, &g2);
+// }
+
+void run_experimentation() {
+	// load min, max, discard, repetitions, samples and initial subject values
+	// incremental_experiment_input_int< adj_list_graph<int>> exp1_input(1, 5, 10, 20, 4, adj_list_graph<int>(), "ej2_prune");
+
+	// adj_list_n_incremental_experiment exp1 = adj_list_n_incremental_experiment(&exp1_input);
+
+	// experiment_suite exp_suite;
+
+	// exp_suite.add(&exp1);
+
+	// exp_suite.run();
+}
