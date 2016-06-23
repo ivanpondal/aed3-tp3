ab3581ca0d5f5851b2775d38ca76452d4fabaa08
manumena <manuelmena1993@gmail.com>
2016-06-23 18:30:52 -0300

    Exercise 2 experimentation.cpp bases

--- src/ej2/experimentation.cpp
+++ src/ej2/experimentation.cpp
@@ -1,25 +1,24 @@
-// #include "../main.h"
-// #include "./ej2.h"
+#include "../main.h"
+#include "./ej2.h"
 
-// void adj_list_n_incremental_experiment::load_instance(incremental_experiment_input<int, adj_list_graph<int>> *input){
-// 	element_generator_int e_gen1, e_gen2;
-// 	graph_factory<int>::add_n_random_vertices(g1, e_gen1, delta_val, 0.5f);
-// 	graph_factory<int>::add_n_random_vertices(g2, e_gen2, delta_val, 0.5f);
-// }
+void mcs_incremental_experiment::load_instance(incremental_experiment_input<int, adj_list_graph<int>> *input){
+	graph_factory<int>::add_n_random_vertices(g1, e_gen1, input->get_delta(), 0.5f);
+	graph_factory<int>::add_n_random_vertices(g2, e_gen2, input->get_delta(), 0.5f);
+}
 
-// void adj_list_n_incremental_experiment::solve_instance(incremental_experiment_input<int, adj_list_graph<int>> *input){
-// 	mcs(&g1, &g2);
-// }
+void mcs_incremental_experiment::solve_instance(incremental_experiment_input<int, adj_list_graph<int>> *input){
+	mcs(&g1, &g2);
+}
 
 void run_experimentation() {
 	// load min, max, discard, repetitions, samples and initial subject values
-	// incremental_experiment_input_int< adj_list_graph<int>> exp1_input(1, 5, 10, 20, 4, adj_list_graph<int>(), "ej2_prune");
+	incremental_experiment_input_int< adj_list_graph<int>> exp1_input(1, 10, 10, 20, 4, adj_list_graph<int>(), "ej2_prune");
 
-	// adj_list_n_incremental_experiment exp1 = adj_list_n_incremental_experiment(&exp1_input);
+	mcs_incremental_experiment exp1 = mcs_incremental_experiment(&exp1_input);
 
-	// experiment_suite exp_suite;
+	experiment_suite exp_suite;
 
-	// exp_suite.add(&exp1);
+	exp_suite.add(&exp1);
 
-	// exp_suite.run();
+	exp_suite.run();
 }
