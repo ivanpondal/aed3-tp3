dd6daab54f98e2f4b0519e0e0d4e7f74561c43b0
manumena <manuelmena1993@gmail.com>
2016-06-22 21:24:24 -0300

    Exercise 2 experimentation base class

--- src/ej2/ej2.h
+++ src/ej2/ej2.h
@@ -7,22 +7,23 @@ using namespace std;
 
 #include <sstream>
 
-// #include "../framework/experiments/incremental_experiment.h"
-// #include "../framework/experiments/experiment_suite.h"
-// #include "../framework/experiments/input/incremental_experiment_input_int.h"
+#include "../framework/experiments/incremental_experiment.h"
+#include "../framework/experiments/experiment_suite.h"
+#include "../framework/experiments/input/incremental_experiment_input_int.h"
 #include "../framework/structures/graph.h"
 #include "../framework/structures/adj_list_graph.h"
 #include "../framework/utils/graph_factory.h"
 #include "../framework/utils/element_generator_int.h"
 
-// class adj_list_n_incremental_experiment: public incremental_experiment<int, adj_list_graph<int>>{
-// 	public:
-// 		adj_list_n_incremental_experiment(const incremental_experiment_input<int, adj_list_graph<int>> *input): incremental_experiment(input){};
-// 	private:
-// 		void load_instance(incremental_experiment_input<int, adj_list_graph<int>> *input);
-// 		void solve_instance(incremental_experiment_input<int, adj_list_graph<int>> *input);
-// 		adj_list_graph<int> g1, g2;
-// };
+class mcs_incremental_experiment: public incremental_experiment<int, adj_list_graph<int>>{
+	public:
+		mcs_incremental_experiment(const incremental_experiment_input<int, adj_list_graph<int>> *input): incremental_experiment(input){};
+	private:
+		void load_instance(incremental_experiment_input<int, adj_list_graph<int>> *input);
+		void solve_instance(incremental_experiment_input<int, adj_list_graph<int>> *input);
+		adj_list_graph<int> g1, g2;
+		element_generator_int e_gen1, e_gen2;
+};
 
 struct hash_tuple_int{
 	size_t operator()(const std::tuple<int, int, int, int> &t) const{
