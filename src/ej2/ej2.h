25fbdb0530c395c3cef4a298b017ae0fca82b2e0
manumena <manuelmena1993@gmail.com>
2016-07-10 21:51:17 -0300

    Exercise 2 experimentation problem

--- src/ej2/ej2.h
+++ src/ej2/ej2.h
@@ -17,15 +17,37 @@
 
 using namespace std;
 
-class mcs_incremental_experiment: public incremental_experiment<int, adj_list_graph<int>>{
+class equal_complete_vs_complete: public incremental_experiment<int, adj_list_graph<int>>{
 	public:
-		mcs_incremental_experiment(const incremental_experiment_input<int, adj_list_graph<int>> *input):
+		equal_complete_vs_complete(const incremental_experiment_input<int, adj_list_graph<int>> *input):
 		                           incremental_experiment(input){};
 	private:
 		void load_instance(incremental_experiment_input<int, adj_list_graph<int>> *input);
 		void solve_instance(incremental_experiment_input<int, adj_list_graph<int>> *input);
 		adj_list_graph<int> g1, g2;
 		element_generator_int e_gen1, e_gen2;
+};
+
+class equal_tree_vs_tree: public incremental_experiment<int, adj_list_graph<int>>{
+    public:
+        equal_tree_vs_tree(const incremental_experiment_input<int, adj_list_graph<int>> *input):
+                                   incremental_experiment(input){};
+    private:
+        void load_instance(incremental_experiment_input<int, adj_list_graph<int>> *input);
+        void solve_instance(incremental_experiment_input<int, adj_list_graph<int>> *input);
+        adj_list_graph<int> g1, g2;
+        element_generator_int e_gen1, e_gen2;
+};
+
+class equal_cycle_vs_cycle: public incremental_experiment<int, adj_list_graph<int>>{
+    public:
+        equal_cycle_vs_cycle(const incremental_experiment_input<int, adj_list_graph<int>> *input):
+                                   incremental_experiment(input){};
+    private:
+        void load_instance(incremental_experiment_input<int, adj_list_graph<int>> *input);
+        void solve_instance(incremental_experiment_input<int, adj_list_graph<int>> *input);
+        adj_list_graph<int> g1, g2;
+        element_generator_int e_gen1, e_gen2;
 };
 
 #endif  // AED3_TP3_EJ2_H
--- src/ej2/experimentation.cpp
+++ src/ej2/experimentation.cpp
@@ -1,23 +1,84 @@
 #include "./ej2.h"
 
-void mcs_incremental_experiment::load_instance(incremental_experiment_input<int, adj_list_graph<int>> *input){
-	graph_factory<int>::add_n_random_vertices(g1, e_gen1, input->get_delta(), 0.5f);
-	graph_factory<int>::add_n_random_vertices(g2, e_gen2, input->get_delta(), 0.5f);
+void equal_complete_vs_complete::load_instance(
+    incremental_experiment_input<int, adj_list_graph<int>> *input) {
+	graph_factory<int>::add_n_vertices_and_all_edges(
+        g1, e_gen1, input->get_delta());
+	graph_factory<int>::add_n_vertices_and_all_edges(
+        g2, e_gen2, input->get_delta());
 }
 
-void mcs_incremental_experiment::solve_instance(incremental_experiment_input<int, adj_list_graph<int>> *input){
-	solve_backtracking(&g1, &g2);
+void equal_complete_vs_complete::solve_instance(
+    incremental_experiment_input<int, adj_list_graph<int>> *input) {
+    cout << g1.n() << endl;
+    solve_backtracking(&g1, &g2);
+}
+
+void equal_tree_vs_tree::load_instance(
+    incremental_experiment_input<int, adj_list_graph<int>> *input) {
+    graph_factory<int>::add_n_tree_vertices(g1, e_gen1, input->get_delta());
+    graph_factory<int>::add_n_tree_vertices(g2, e_gen2, input->get_delta());
+}
+
+void equal_tree_vs_tree::solve_instance(
+    incremental_experiment_input<int, adj_list_graph<int>> *input) {
+    cout << g1.n() << endl;
+    solve_backtracking(&g1, &g2);
+}
+
+void equal_cycle_vs_cycle::load_instance(
+    incremental_experiment_input<int, adj_list_graph<int>> *input) {
+    e_gen1.reset();
+    e_gen2.reset();
+    g1 = graph_factory<int>::cycle_graph(e_gen1, input->get_delta());
+    g2 = graph_factory<int>::cycle_graph(e_gen2, input->get_delta());
+}
+
+void equal_cycle_vs_cycle::solve_instance(
+    incremental_experiment_input<int, adj_list_graph<int>> *input) {
+    cout << g1.n() << endl;
+    solve_backtracking(&g1, &g2);
 }
 
 void run_experimentation() {
 	// load min, max, discard, repetitions, samples and initial subject values
-	incremental_experiment_input_int< adj_list_graph<int>> exp1_input(1, 10, 10, 20, 4, adj_list_graph<int>(), "ej2_prune");
-
-	mcs_incremental_experiment exp1 = mcs_incremental_experiment(&exp1_input);
-
+	incremental_experiment_input_int< adj_list_graph<int>> equal_complete_input(
+        1,
+        7,
+        0,
+        3,
+        7,
+        adj_list_graph<int>(),
+        "../exp/ej2/equal_complete_vs_complete");
+	equal_complete_vs_complete equal_complete = equal_complete_vs_complete(
+        &equal_complete_input);
 	experiment_suite exp_suite;
+	exp_suite.add(&equal_complete);
 
-	exp_suite.add(&exp1);
+    incremental_experiment_input_int< adj_list_graph<int>> equal_tree_input(
+        1,
+        7,
+        0,
+        3,
+        7,
+        adj_list_graph<int>(),
+        "../exp/ej2/equal_tree_vs_tree");
+    equal_tree_vs_tree equal_tree = equal_tree_vs_tree(&equal_tree_input);
+    experiment_suite exp_suite;
+    exp_suite.add(&equal_tree);
+
+    // ESTE ES EL QUE ESTA JODIENDO, NO AUMENTA LA CANTIDAD DE NODOS
+    incremental_experiment_input_int< adj_list_graph<int>> equal_cycle_input(
+        1,
+        7,
+        0,
+        3,
+        7,
+        adj_list_graph<int>(),
+        "../exp/ej2/equal_cycle_vs_cycle");
+    equal_cycle_vs_cycle equal_cycle = equal_cycle_vs_cycle(&equal_cycle_input);
+    experiment_suite exp_suite;
+exp_suite.add(&equal_cycle);
 
 	exp_suite.run();
 }
