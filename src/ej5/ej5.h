a15266d008ed2b8769ebbe633842a2d9a2377758
darkBasic666 <m4xileon@gmail.com>
2016-07-11 16:20:00 -0300

    added experiments

--- src/ej5/ej5.h
+++ src/ej5/ej5.h
@@ -1,10 +1,17 @@
 #ifndef AED3_TP3_EJ5_H
 #define AED3_TP3_EJ5_H
+#include <utility>
 
 #include "../main.h"
 #include "../heuristics/heuristics.h"
 #include "../framework/experiments/experiment_suite.h"
 #include "../framework/experiments/quality_experiment.h"
+#include "../framework/utils/element_generator_int.h"
+#include "../framework/utils/graph_factory.h"
+#include "../framework/experiments/quality_incremental_experiment.h"
+#include "../framework/experiments/input/incremental_experiment_input_int.h"
+#include "../framework/experiments/input/incremental_experiment_input_float.h"
+
 
 /*
 **  Exercise 5
@@ -13,7 +20,13 @@
 // Here go declarations of functions which are exported by the problem solver,
 // global variables, etc.
 
-#endif  // AED3_TP3_EJ5_H
+
+struct star_solution {
+	adj_list_graph<int> g1;
+	adj_list_graph<int> g2;
+	graph<std::pair<int, int>> *greedy_solve;
+};
+
 
 // Local search , neighbourhood without swap
 
@@ -26,5 +39,38 @@ class quality_exp_local_search_without_swap: public quality_experiment {
 	private:
 		unsigned int solve_instance(graph<int> &g1, graph<int> &g2);
 		void clean_solution();
-		graph<std::pair<int, int>>* solution_graph_ptr;
+		graph<std::pair<int, int>>* h;
 };
+
+// Local search , neighbourhood with swap
+
+
+class quality_exp_local_search_with_swap: public quality_experiment {
+	public:
+		quality_exp_local_search_with_swap(std::vector<std::string> instances, const std::string &output_file_name,
+		                   const int discard_val, const int repetitions_val):
+		quality_experiment(instances, generated_instances_directory,
+		                   output_file_name, discard_val, repetitions_val){};
+	private:
+		unsigned int solve_instance(graph<int> &g1, graph<int> &g2);
+		void clean_solution();
+		graph<std::pair<int, int>>* h;
+};
+
+class g1_vs_g2_neighbourhood_1_proportion_incremental_experiment:public quality_incremental_experiment< float,
+	 star_solution >{
+    public:
+        g1_vs_g2_neighbourhood_1_proportion_incremental_experiment(const incremental_experiment_input< float, 
+        	star_solution > *input): quality_incremental_experiment(input){
+
+        };
+    private:
+        void load_instance(incremental_experiment_input< float, star_solution > *input);
+        int solve_instance(incremental_experiment_input< float, star_solution >*input);
+};
+
+
+
+
+
+#endif  // AED3_TP3_EJ5_H
--- src/ej5/experimentation.cpp
+++ src/ej5/experimentation.cpp
@@ -12,39 +12,261 @@
 //     graph<std::pair<int, int>>& start_point,
 //     int neighbourhood_type,                   // 1 or 2
 //     int iteration_limit = -1,                 // -1 indicates no limit
-//     // int neighbourhood_limit = -1,          // discarded parameter
 //     float neighbourhood_proportion = 1,       // between 0 and 1
 //     bool strict_comparisons = true
 // );
 
 using namespace std;
 
-unsigned int quality_exp_local_search_without_swap::solve_instance(graph<int> &g1, graph<int> &g2) {
-	graph<pair<int, int>> *start_point = solve_greedy(g1, g2);
-    solution_graph_ptr= solve_local_search(g1, g2, *start_point, 1);
+unsigned int quality_exp_local_search_with_swap::solve_instance(graph<int> &g1, graph<int> &g2) {
 
-	return solution_graph_ptr->m();
+	graph<int>* large_graph, * small_graph;
+
+    // Local search solver assumes g1 has at most as many nodes as g2. If that
+    // is not the case, we have to exchange both graphs before passing them
+    // to the function, and remember this fact so that we can read correctly
+    // the obtained solution
+    if (g1.n() <= g2.n()) {
+        small_graph = &g1;
+        large_graph = &g2;
+    } else {
+        small_graph = &g2;
+        large_graph = &g1;
+    }
+
+    graph<pair<int, int>>* start_point = solve_greedy(*small_graph, *large_graph);
+    h = solve_local_search(*small_graph, *large_graph, *start_point, 2,5,0.05f,true);
+
+    delete start_point;
+    
+	
+	return h->m();
 }
+
+void quality_exp_local_search_with_swap::clean_solution() {
+	delete h;
+
+}
+
+
+unsigned int quality_exp_local_search_without_swap::solve_instance(graph<int> &g1, graph<int> &g2) {
+
+	graph<int>* large_graph, * small_graph;
+
+    // Local search solver assumes g1 has at most as many nodes as g2. If that
+    // is not the case, we have to exchange both graphs before passing them
+    // to the function, and remember this fact so that we can read correctly
+    // the obtained solution
+    if(	g1.n() == g2.n() ){
+    
+    	h = solve_greedy(g1,g2);
+    
+    }else{
+
+    	if (g1.n() <= g2.n()) {
+	        small_graph = &g1;
+	        large_graph = &g2;
+	    } else {
+	        small_graph = &g2;
+	        large_graph = &g1;
+	    }
+	    graph<pair<int, int>>* start_point = solve_greedy(*small_graph, *large_graph);
+    	h = solve_local_search(*small_graph, *large_graph, *start_point, 1,5,0.05f,true);
+    	delete start_point;
+    }
+   
+
+    
+
+    
+    
+	
+	return h->m();
+}
+
+
 
 void quality_exp_local_search_without_swap::clean_solution() {
-	delete solution_graph_ptr;
+	delete h;
 
 }
 
+void g1_vs_g2_neighbourhood_1_proportion_incremental_experiment::load_instance(incremental_experiment_input<float,
+	star_solution >*input) {
+	cout << "neighbourhood_proportion :  " << input->get_inc_val() << endl;
+	// cout << "g1.n() :  " << input->get_subject().g1.n()<< endl;
+	// cout << "g2.n() :  " << input->get_subject().g2.n()<< endl;
 
+}
+
+int g1_vs_g2_neighbourhood_1_proportion_incremental_experiment::solve_instance(incremental_experiment_input<float,
+	star_solution > *input){
+	// solve_local_search(graph<int>&, graph<int>&, graph<std::pair<int, int> >&, int, int, float, bool)
+		graph<pair<int, int>>* h = solve_local_search(input->get_subject().g1, input->get_subject().g2,
+		*input->get_subject().greedy_solve,1,-1,input->get_inc_val(),true);
+	cout << "h->m() :  " << h->m() << endl;
+	return h->m();
+
+}
 
 // Experiments
 
 void run_experimentation(){
+
+
 	experiment_suite quality_exp_suite;
-	int repetitions_val = 50;
+	experiment_suite  neighbourhood_proportion_calibrate_exp_suite;
+	
+	// quality exp
+	int repetitions_val = 1;
 
-	quality_exp_local_search_without_swap greedy_vs_local_search_1_exp(
-			greedy_solution_instances, "../exp/ej5/greedy_vs_local_search_1_exp", 0, repetitions_val);
+	// // quality local_search_2_exp
+	// quality_exp_local_search_with_swap local_search_2_exp(
+	// 		known_solution_instances, "../exp/ej5/known_solution_instances_search_2_exp", 0, repetitions_val);
 
-	quality_exp_suite.add(&greedy_vs_local_search_1_exp);
 
-	quality_exp_suite.run();
+	// // quality_exp_suite.add(&local_search_2_exp);
+
+	// // quality local_search_1_exp
+	// quality_exp_local_search_with_swap local_search_1_exp(
+	// 		known_solution_instances, "../exp/ej5/known_solution_instances_search_1_exp", 0, repetitions_val);
+
+
+	// quality_exp_suite.add(&local_search_1_exp);
+
+	// quality_exp_suite.run();
+
+
+
+	// neighbourhood proportion calibrate
+	
+	// neighbourhood 1
+	element_generator_int e_gen;
+	// generate graph families
+	adj_list_graph<int> big_tree;
+	graph_factory<int>::add_n_tree_vertices(big_tree, e_gen, 800);
+	e_gen.reset();
+	adj_list_graph<int> small_tree;
+	graph_factory<int>::add_n_tree_vertices(small_tree, e_gen, 400);
+	
+	e_gen.reset();
+	adj_list_graph<int> complete;
+	graph_factory<int>::add_n_vertices_and_all_edges(complete, e_gen, 400);
+	e_gen.reset();
+	adj_list_graph<int> big_cicle = graph_factory<int>::cycle_graph(e_gen, 800);
+	adj_list_graph<int> small_cicle = graph_factory<int>::cycle_graph(e_gen, 400);
+	star_solution parameters;
+	graph<pair<int, int>>* start_point;
+
+
+
+	// big_tree_vs_small_tree 
+	start_point = solve_greedy(small_tree,big_tree);
+
+	parameters.g1 =  small_tree;
+	parameters.g2 =  big_tree;
+	parameters.greedy_solve = start_point;
+
+	incremental_experiment_input_float< star_solution >  big_tree_vs_small_tree_input(0.0f, 1.0f, 3, 5,
+		10, parameters, "../exp/ej5/big_tree_vs_small_tree_neighbourhood_1_proportion");
+
+    g1_vs_g2_neighbourhood_1_proportion_incremental_experiment  big_tree_vs_small_tree_exp = 
+    	g1_vs_g2_neighbourhood_1_proportion_incremental_experiment(&big_tree_vs_small_tree_input);
+
+   
+
+    // big_cicle_vs_small_cicle
+	start_point = solve_greedy(small_cicle,big_cicle);
+
+	parameters.g1 =  small_cicle;
+	parameters.g2 =  big_cicle;
+	parameters.greedy_solve = start_point;
+
+	incremental_experiment_input_float< star_solution >  big_cicle_vs_small_cicle_input(0.0f, 1.0f, 3, 5,
+		10, parameters, "../exp/ej5/big_cicle_vs_small_cicle_neighbourhood_1_proportion");
+
+    g1_vs_g2_neighbourhood_1_proportion_incremental_experiment  big_cicle_vs_small_cicle_exp = 
+    	g1_vs_g2_neighbourhood_1_proportion_incremental_experiment(&big_cicle_vs_small_cicle_input);
+
+
+
+	// tree_vs_complete 
+	start_point = solve_greedy(complete,big_tree);
+
+	parameters.g1 =  complete;
+	parameters.g2 =  big_tree;
+	parameters.greedy_solve = start_point;
+
+	incremental_experiment_input_float< star_solution >  tree_vs_complete_input(0.0f, 1.0f, 3, 5,
+		10, parameters, "../exp/ej5/tree_vs_complete_neighbourhood_1_proportion");
+
+    g1_vs_g2_neighbourhood_1_proportion_incremental_experiment tree_vs_complete_exp = 
+    	g1_vs_g2_neighbourhood_1_proportion_incremental_experiment(&tree_vs_complete_input);
+
+    // cicle_vs_complete 
+    start_point = solve_greedy(complete,big_cicle);
+
+
+	parameters.g1 =  complete;
+	parameters.g2 =  big_cicle;
+	parameters.greedy_solve = start_point;
+
+	incremental_experiment_input_float< star_solution >  cicle_vs_complete_input(0.0f, 1.0f, 3, 5,
+		10, parameters, "../exp/ej5/cicle_vs_complete_neighbourhood_1_proportion");
+
+    g1_vs_g2_neighbourhood_1_proportion_incremental_experiment cicle_vs_complete_exp = 
+    	g1_vs_g2_neighbourhood_1_proportion_incremental_experiment(&cicle_vs_complete_input);
+
+ 
+
+    //big_tree_vs_small_cicle_exp
+
+    start_point = solve_greedy(small_cicle,big_tree);
+
+    parameters.g1 =  small_cicle;
+	parameters.g2 =  big_tree;
+	parameters.greedy_solve = start_point;
+
+	incremental_experiment_input_float< star_solution >  big_tree_vs_small_cicle_input(0.0f, 1.0f, 3, 5,
+		10, parameters, "../exp/ej5/big_tree_vs_small_cicle_neighbourhood_1_proportion");
+
+    g1_vs_g2_neighbourhood_1_proportion_incremental_experiment big_tree_vs_small_cicle_exp = 
+    	g1_vs_g2_neighbourhood_1_proportion_incremental_experiment(&big_tree_vs_small_cicle_input);
+
+    // big_cicle_vs_small_tree_exp
+
+     start_point = solve_greedy(small_tree,big_tree);
+
+    parameters.g1 =  small_tree;
+	parameters.g2 =  big_cicle;
+	parameters.greedy_solve = start_point;
+
+	incremental_experiment_input_float< star_solution >  big_cicle_vs_small_tree_input(0.0f, 1.0f, 3, 5,
+		10, parameters, "../exp/ej5/big_cicle_vs_small_tree_1_proportion");
+
+    g1_vs_g2_neighbourhood_1_proportion_incremental_experiment big_cicle_vs_small_tree_exp = 
+    	g1_vs_g2_neighbourhood_1_proportion_incremental_experiment(&big_cicle_vs_small_tree_input);
+
+
+    //neighbourhood_proportion_calibrate_exp_suite.add(&big_tree_vs_small_tree_exp);
+
+   	//neighbourhood_proportion_calibrate_exp_suite.add(&big_cicle_vs_small_cicle_exp);
+
+    //neighbourhood_proportion_calibrate_exp_suite.add(&cicle_vs_complete_exp);
+    
+    //neighbourhood_proportion_calibrate_exp_suite.add(&tree_vs_complete_exp);
+
+    //neighbourhood_proportion_calibrate_exp_suite.add(&big_tree_vs_small_cicle_exp);
+
+    //neighbourhood_proportion_calibrate_exp_suite.add(&big_cicle_vs_small_tree_exp);
+
+    
+
+
+
+    neighbourhood_proportion_calibrate_exp_suite.run();
+
+
 	
 
 }
