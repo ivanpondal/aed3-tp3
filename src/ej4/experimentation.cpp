5ee691f5b5967f3a89f3a9e374bdb28b82e98193
Iván Pondal <ivan.pondal@gmail.com>
2016-07-14 19:44:06 -0300

    Add exercise 4 experiment generators

--- src/ej4/experimentation.cpp
+++ src/ej4/experimentation.cpp
@@ -24,11 +24,70 @@ void quality_exp_greedy_add_neighbours::clean_solution() {
 	delete solution_graph_ptr;
 }
 
+// Complexity
+
+void inc_exp_add_nodes::load_instance(
+	incremental_experiment_input<int, adj_list_graph<int>> *input
+) {
+	int new_element;
+	for (int i = 0; i < input->get_delta(); i++) {
+		new_element = e_gen.generate(g1);
+		g1.add_node(new_element);
+		g2.add_node(new_element);
+	}
+}
+
+void inc_exp_add_nodes::solve_instance(
+	incremental_experiment_input<int, adj_list_graph<int>> *input
+) {
+	sol = solve_greedy(g1, g2);
+}
+
+void inc_exp_add_nodes::clean_solution() {
+	delete sol;
+}
+
+void inc_exp_add_tree_nodes::load_instance(
+	incremental_experiment_input<int, adj_list_graph<int>> *input
+) {
+	graph_factory<int>::add_n_tree_vertices(g1, e_gen_1, input->get_delta());
+	graph_factory<int>::add_n_tree_vertices(g2, e_gen_2, input->get_delta());
+}
+
+void inc_exp_add_tree_nodes::solve_instance(
+	incremental_experiment_input<int, adj_list_graph<int>> *input
+) {
+	sol = solve_greedy(g1, g2);
+}
+
+void inc_exp_add_tree_nodes::clean_solution() {
+	delete sol;
+}
+
+void inc_exp_add_complete_nodes::load_instance(
+	incremental_experiment_input<int, adj_list_graph<int>> *input
+) {
+	graph_factory<int>::add_n_vertices_and_all_edges(g1, e_gen_1, input->get_delta());
+	graph_factory<int>::add_n_vertices_and_all_edges(g2, e_gen_2, input->get_delta());
+}
+
+void inc_exp_add_complete_nodes::solve_instance(
+	incremental_experiment_input<int, adj_list_graph<int>> *input
+) {
+	sol = solve_greedy(g1, g2);
+}
+
+void inc_exp_add_complete_nodes::clean_solution() {
+	delete sol;
+}
+
 // Experiments
 
 void run_experimentation(){
-	experiment_suite quality_exp_suite;
+	experiment_suite exp_suite;
 	int repetitions_val = 50;
+
+	// Quality
 
 	quality_exp_greedy known_sol_greedy_exp(
 			known_solution_instances, "../exp/ej4/known_sol_greedy_exp", 0, repetitions_val);
@@ -40,10 +99,34 @@ void run_experimentation(){
 	quality_exp_greedy_add_neighbours unknown_sol_greedy_add_neighbours_exp(
 			unknown_solution_instances, "../exp/ej4/unknown_sol_greedy_add_neighbours_exp", 0, repetitions_val);
 
-	quality_exp_suite.add(&known_sol_greedy_exp);
-	quality_exp_suite.add(&unknown_sol_greedy_exp);
-	quality_exp_suite.add(&known_sol_greedy_add_neighbours_exp);
-	quality_exp_suite.add(&unknown_sol_greedy_add_neighbours_exp);
+	// Complexity
 
-	quality_exp_suite.run();
+	incremental_experiment_input_int< adj_list_graph<int>> input_add_nodes_exp(
+		1, 30000, 0, 20, 250, adj_list_graph<int>(), "../exp/ej4/add_nodes_exp"
+	);
+
+	inc_exp_add_nodes add_nodes_exp(&input_add_nodes_exp);
+
+	incremental_experiment_input_int< adj_list_graph<int>> input_add_tree_nodes_exp(
+		1, 55000, 0, 30, 100, adj_list_graph<int>(), "../exp/ej4/add_tree_nodes_exp"
+	);
+
+	inc_exp_add_tree_nodes add_tree_nodes_exp(&input_add_tree_nodes_exp);
+
+	incremental_experiment_input_int< adj_list_graph<int>> input_add_complete_nodes_exp(
+		1, 1000, 0, 30, 100, adj_list_graph<int>(), "../exp/ej4/add_complete_nodes_exp"
+	);
+
+	inc_exp_add_complete_nodes add_complete_nodes_exp(&input_add_complete_nodes_exp);
+
+	exp_suite.add(&known_sol_greedy_exp);
+	exp_suite.add(&unknown_sol_greedy_exp);
+	exp_suite.add(&known_sol_greedy_add_neighbours_exp);
+	exp_suite.add(&unknown_sol_greedy_add_neighbours_exp);
+
+	exp_suite.add(&add_nodes_exp);
+	exp_suite.add(&add_tree_nodes_exp);
+	exp_suite.add(&add_complete_nodes_exp);
+
+	exp_suite.run();
 }
